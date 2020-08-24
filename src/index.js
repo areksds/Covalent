fcd773e71b59ac49f58e313a79b851b80334abb6
mykull <69874869+mykulls@users.noreply.github.com>
2020-08-24 01:36:31 -0700

    fixing build error
    ./Menu --> ./Components/Menu

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import Menu from './Menu';
+import Menu from './Components/Menu';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
