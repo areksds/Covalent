d9f5ae52eb7467036922a2cac7968627ff40573b
mykull <69874869+mykulls@users.noreply.github.com>
2020-08-24 01:22:58 -0700

    importing Menu

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import Menu from './Menu';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Menu />
   </React.StrictMode>,
   document.getElementById('root')
 );
